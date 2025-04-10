de8130009b4280f50387e5aa6c4219eada84efeb
arboreal <samuel.ehrick@gmail.com>
2025-04-09 21:43:46 -0400

    styled main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
-from textnode import *
 from htmlnode import HTMLNode
+from textnode import *
+
 
 def main():
     text_node = TextNode("This is a text node", TextType.BOLD, "https://www.win.com")
-    html_node = HTMLNode("a", "this is a test htmlnode", None, {"href": "https://www.test.com", "target": "_blank"})
+    html_node = HTMLNode(
+        "a",
+        "this is a test htmlnode",
+        None,
+        {"href": "https://www.test.com", "target": "_blank"},
+    )
 
     print(text_node)
     print(html_node.props_to_html())
-
 
 
 main()
