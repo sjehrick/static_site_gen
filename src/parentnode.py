84c803650dea8fd4da79f4448ce155d064ff6598
arboreal <samuel.ehrick@gmail.com>
2025-08-23 15:43:13 -0400

    created parentnode.py and associated parentnode unit tests

---
+++ src/parentnode.py
@@ -0,0 +1,23 @@
+from htmlnode import HTMLNode
+from leafnode import LeafNode
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Parent node must have a tag")
+
+        if self.children is None:
+            raise ValueError("Parent node must have children")
+
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
