506c72dac2b78a30f5f6115400f2a5d90b93cd2b
arboreal <samuel.ehrick@gmail.com>
2025-08-26 18:04:56 -0400

    created new_nodes variable and logic to filter non TextType.Text nodes into the final list

--- src/split_nodes_delimiter.py
+++ src/split_nodes_delimiter.py
@@ -1,3 +1,10 @@
 from textnode import TextType, TextNode
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+
+
