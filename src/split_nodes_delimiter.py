cde59a763f608f84c2a5aa81ee1a69d57f1e0e08
arboreal <samuel.ehrick@gmail.com>
2025-08-27 13:04:57 -0400

    Created logic to check for matched sets of delimiters and raise an error if mismatches are found

--- src/split_nodes_delimiter.py
+++ src/split_nodes_delimiter.py
@@ -1,10 +1,14 @@
-from textnode import TextType, TextNode
+from textnode import TextNode, TextType
+
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
-    
+
     for node in old_nodes:
         if node.text_type != TextType.TEXT:
             new_nodes.append(node)
 
-
+    for node in old_nodes:
+        if node.text_type == TextType.Text:
+            if node.text.count(delimiter) % 2 != 0:
+                raise ValueError("Error: matching closing delimiter not found. Did you forget to add a {delimiter} in your markdown?")
