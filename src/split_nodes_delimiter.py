1e1a5c0df21e26486329bb27a1617625625fc26a
arboreal <samuel.ehrick@gmail.com>
2025-08-28 20:48:39 -0400

    added initial text splitting logic inside TextType.Text node for loop

--- src/split_nodes_delimiter.py
+++ src/split_nodes_delimiter.py
@@ -9,6 +9,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             new_nodes.append(node)
 
     for node in old_nodes:
+        working_node_text = []
+
         if node.text_type == TextType.Text:
             if node.text.count(delimiter) % 2 != 0:
                 raise ValueError("Error: matching closing delimiter not found. Did you forget to add a {delimiter} in your markdown?")
+            
+            working_node_text.append(node.text.split(delimiter))
+    
