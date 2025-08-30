c606d299493fe0c2ead1602264afb8c305f567c0
arboreal <samuel.ehrick@gmail.com>
2025-08-29 21:18:35 -0400

    added for loop to generate text nodes by index of the split list text of the proper TextType

--- src/split_nodes_delimiter.py
+++ src/split_nodes_delimiter.py
@@ -16,4 +16,9 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 raise ValueError("Error: matching closing delimiter not found. Did you forget to add a {delimiter} in your markdown?")
             
             working_node_text.append(node.text.split(delimiter))
-    
+        
+        for i in range(len(working_node_text)):
+            if working_node_text[i] % 2 == 1:
+                new_nodes.append(TextNode(working_node_text[i], text_type))
+            else:
+                new_nodes.append(TextNode(working_node_text[i], TextType.TEXT))
