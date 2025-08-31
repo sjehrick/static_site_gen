ddf0a4de9fcb813d89c15100d42e8b17dc2751f3
arboreal <samuel.ehrick@gmail.com>
2025-08-31 15:17:00 -0400

    debugged multiple inline splits and splitting one delimiter followed by another

--- src/split_nodes_delimiter.py
+++ src/split_nodes_delimiter.py
@@ -7,18 +7,19 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     for node in old_nodes:
         if node.text_type != TextType.TEXT:
             new_nodes.append(node)
-
-    for node in old_nodes:
+            continue
         working_node_text = []
-
-        if node.text_type == TextType.Text:
-            if node.text.count(delimiter) % 2 != 0:
-                raise ValueError("Error: matching closing delimiter not found. Did you forget to add a {delimiter} in your markdown?")
-            
-            working_node_text.append(node.text.split(delimiter))
+        sections = node.text.split(delimiter)
         
-        for i in range(len(working_node_text)):
-            if working_node_text[i] % 2 == 1:
-                new_nodes.append(TextNode(working_node_text[i], text_type))
+        if len(sections) % 2 == 0:
+            raise ValueError(f"Error: matching closing delimiter not found. Did you forget to add a {delimiter} in your markdown?")
+            
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                working_node_text.append(TextNode(sections[i], TextType.TEXT))
             else:
-                new_nodes.append(TextNode(working_node_text[i], TextType.TEXT))
+                working_node_text.append(TextNode(sections[i], text_type))
+        new_nodes.extend(working_node_text)
+    return new_nodes
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,8 @@
 import unittest
 
-from textnode import TextNode, TextType
 from leafnode import LeafNode
+from split_nodes_delimiter import split_nodes_delimiter
+from textnode import TextNode, TextType
 from textnode_to_htmlnode import text_node_to_html_node
 
 
@@ -10,7 +11,7 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
-    
+
     def test_texttype_not_eq(self):
         node = TextNode("This is a text node", TextType.CODE)
         node2 = TextNode("This is a text node", TextType.BOLD)
@@ -18,7 +19,9 @@ class TestTextNode(unittest.TestCase):
 
     def test_url_not_eq(self):
         node = TextNode("This is a text node", TextType.TEXT)
-        node2 = TextNode("This is a text node", TextType.TEXT, "https://superawesome.io")
+        node2 = TextNode(
+            "This is a text node", TextType.TEXT, "https://superawesome.io"
+        )
         self.assertNotEqual(node, node2)
 
     def test_type_eq(self):
@@ -30,13 +33,13 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a node", TextType.LINK, "https://codingisfun.com")
         node2 = TextNode("This is a node", TextType.LINK, "https://codingisfun.com")
         self.assertEqual(node, node2)
-        
+
     def test_text(self):
         node = TextNode("This is a text node", TextType.TEXT)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
-    
+
     def test_text_bold(self):
         node = TextNode("This is a bold text node", TextType.BOLD)
         html_node = text_node_to_html_node(node)
@@ -54,20 +57,40 @@ class TestTextNode(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "code")
         self.assertEqual(html_node.value, "This is a code text node")
-    
+
     def test_text_link(self):
-        node = TextNode("This is a link text node", TextType.LINK, "https://www.test.com")
+        node = TextNode(
+            "This is a link text node", TextType.LINK, "https://www.test.com"
+        )
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "a")
         self.assertEqual(html_node.value, "This is a link text node")
         self.assertEqual(html_node.props, {"href": "https://www.test.com"})
-    
+
     def test_text_image(self):
-        node = TextNode("This is an image text node", TextType.IMAGE, "https://www.imagetest.com")
+        node = TextNode(
+            "This is an image text node", TextType.IMAGE, "https://www.imagetest.com"
+        )
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "img")
         self.assertEqual(html_node.value, "")
-        self.assertEqual(html_node.props, {"src": "https://www.imagetest.com", "alt": "This is an image text node"})
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.imagetest.com", "alt": "This is an image text node"},
+        )
+
+    def test_split_code(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" word", TextType.TEXT),
+            ],
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
