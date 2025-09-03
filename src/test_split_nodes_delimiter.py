e433192f55bc9b93688eebae9385433ee76a662d
arboreal <samuel.ehrick@gmail.com>
2025-09-02 23:13:03 -0400

    extract images and links functions plus tests

--- src/test_split_nodes_delimiter.py
+++ src/test_split_nodes_delimiter.py
@@ -1,4 +1,6 @@
 import unittest
+
+from inline_markdown import extract_markdown_images, extract_markdown_links
 from split_nodes_delimiter import split_nodes_delimiter
 from textnode import TextNode, TextType
 
@@ -83,6 +85,19 @@ class TestInlineMarkdown(unittest.TestCase):
             new_nodes,
         )
 
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual(
+            [("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertListEqual(
+            [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
 
 if __name__ == "__main__":
     unittest.main()
