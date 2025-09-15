d0a826c7b2b6fc5725281a4c78f7e7a7cb2e341b
arboreal <samuel.ehrick@gmail.com>
2025-09-15 09:56:10 -0400

    created text_to_textnodes and associated unit test

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -109,3 +109,12 @@ def split_nodes_link(old_nodes):
 
         new_nodes.extend(parts)
     return new_nodes
+
+def text_to_textnodes(text):
+    initial_textnode = [TextNode(text, TextType.TEXT)]
+    bold_split = split_nodes_delimiter(initial_textnode, "**", TextType.BOLD)
+    italic_split = split_nodes_delimiter(bold_split, "_", TextType.ITALIC)
+    code_split = split_nodes_delimiter(italic_split, "`", TextType.CODE)
+    image_split = split_nodes_image(code_split)
+    link_split = split_nodes_link(image_split)
+    return link_split
