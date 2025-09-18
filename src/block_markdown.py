fbdaae2afa0c4d4fa85d95647aff741512e9d2e8
arboreal <samuel.ehrick@gmail.com>
2025-09-17 22:49:31 -0400

    created BlockType Enum in prep for block to block type function creation

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,13 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered list"
+    ORDERED_LIST = "ordered list"
+
 def markdown_to_blocks(markdown):
     new_blocks = []
 
