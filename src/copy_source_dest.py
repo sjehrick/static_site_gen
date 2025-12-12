9f96d9892c410665af39cdbccad93a703c074489
samuel ehrick <samuel.ehrick@gmail.com>
2025-12-11 21:24:31 -0500

    added initial path logic

--- src/copy_source_dest.py
+++ src/copy_source_dest.py
@@ -1,6 +1,11 @@
 import os
 import shutil
 
-def copy_source_dest(source, dest):
+def copy_source_dest(source_dir, dest_dir):
+    absolute_source = os.path.abspath(source_dir)
+    absolute_dest = os.path.abspath(dest_dir)
 
+    shutil.rmtree(absolute_dest, ignore_errors=True)
+
+    os.mkdir(absolute_dest)
 
