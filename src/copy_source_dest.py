862bb37fb782e0177128d3c9f284c816f51b8d6a
samuel ehrick <samuel.ehrick@gmail.com>
2025-12-12 21:57:21 -0500

    wrote recursive copy function

--- src/copy_source_dest.py
+++ src/copy_source_dest.py
@@ -9,3 +9,19 @@ def copy_source_dest(source_dir, dest_dir):
 
     os.mkdir(absolute_dest)
 
+    dirs = [dir for dir in os.listdir(absolute_source) if os.path.isdir(os.path.join(absolute_source, dir))]
+
+    def recursive_copy(directory):
+        
+        absolute_dir = os.path.abspath(directory)
+
+        files = [file for file in os.listdir(directory) if os.path.isfile(os.path.join(absolute_dir, file))]
+
+        for file in files:
+            print(f"Copying {file} to {absolute_dest}")
+            shutil.copy(file, absolute_dest)
+
+    recursive_copy(absolute_source)
+
+    for dir in dirs:
+        recursive_copy(dir)
