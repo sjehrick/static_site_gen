1ef81a4b690c7b8bd3a12218dc408bf98289f10b
samuel ehrick <samuel.ehrick@gmail.com>
2025-12-13 14:46:46 -0500

    completed recursive copy function

--- src/copy_source_dest.py
+++ src/copy_source_dest.py
@@ -3,25 +3,51 @@ import shutil
 
 def copy_source_dest(source_dir, dest_dir):
     absolute_source = os.path.abspath(source_dir)
+    print(f"absolute source is {absolute_source}")
     absolute_dest = os.path.abspath(dest_dir)
+    print(f"absolute dest is {absolute_dest}")
 
     shutil.rmtree(absolute_dest, ignore_errors=True)
 
     os.mkdir(absolute_dest)
 
-    dirs = [dir for dir in os.listdir(absolute_source) if os.path.isdir(os.path.join(absolute_source, dir))]
+    if os.path.exists(absolute_dest):
+        print(f"{absolute_dest} successfully created")
 
-    def recursive_copy(directory):
-        
-        absolute_dir = os.path.abspath(directory)
+    #dirs = [dir for dir in os.listdir(absolute_source) if os.path.isdir(os.path.join(absolute_source, dir))]
 
-        files = [file for file in os.listdir(directory) if os.path.isfile(os.path.join(absolute_dir, file))]
+    def recursive_copy(src_directory, dest_directory):
+        absolute_dir = os.path.abspath(src_directory)
+        #print(absolute_dir)
+
+        files = [file for file in os.listdir(absolute_dir) if os.path.isfile(os.path.join(absolute_dir, file))]
+
+        nested_dirs = [dir for dir in os.listdir(absolute_dir) if os.path.isdir(os.path.join(absolute_dir, dir))]
 
         for file in files:
-            print(f"Copying {file} to {absolute_dest}")
-            shutil.copy(file, absolute_dest)
+            abs_file = os.path.join(absolute_dir, file)
+            print(f"Copying {abs_file} to {dest_directory}")
+            shutil.copy(abs_file, dest_directory)
 
-    recursive_copy(absolute_source)
+        for dir in nested_dirs:
 
-    for dir in dirs:
-        recursive_copy(dir)
+            new_dest = os.path.abspath(os.path.join(dest_directory, dir))
+            #print(new_dest)
+
+            
+            shutil.rmtree(new_dest, ignore_errors=True)
+
+            os.mkdir(new_dest)
+            
+            if os.path.exists(new_dest):
+                print(f"{new_dest} successfully created")
+
+            abs_dir = os.path.join(absolute_dir, dir)
+            #print(abs_dir)
+
+            recursive_copy(abs_dir, new_dest)
+
+    recursive_copy(absolute_source, absolute_dest)
+
+    #for dir in dirs:
+     #   recursive_copy(dir)
--- src/main.py
+++ src/main.py
@@ -1,18 +1,9 @@
-from htmlnode import HTMLNode
-from textnode import *
+#from htmlnode import HTMLNode
+#from textnode import *
+from copy_source_dest import copy_source_dest
 
 
 def main():
-    text_node = TextNode("This is a text node", TextType.BOLD, "https://www.win.com")
-    html_node = HTMLNode(
-        "a",
-        "this is a test htmlnode",
-        None,
-        {"href": "https://www.test.com", "target": "_blank"},
-    )
-
-    print(text_node)
-    print(html_node.props_to_html())
-
+    copy_source_dest("static", "public")
 
 main()
