1686eece0f7602ba961b8147a29ffa4d114937c4
samuel ehrick <samuel.ehrick@gmail.com>
2025-12-16 20:24:10 -0500

    started generatepage.py

---
+++ src/generatepage.py
@@ -0,0 +1,7 @@
+import os
+from extractmdtitle import extract_title
+from block_markdown import markdown_to_html_node
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
