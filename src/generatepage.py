a623fb8dc1062c42a13ba633bd1d3d355a72966e
samuel ehrick <samuel.ehrick@gmail.com>
2025-12-21 20:56:41 -0500

    finished generate_pages_recursive

--- src/generatepage.py
+++ src/generatepage.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from extractmdtitle import extract_title
 from block_markdown import *
 
@@ -33,3 +34,27 @@ def generate_page(from_path, template_path, dest_path):
 
 def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
     print(f"Generating pages from {dir_path_content} to {dest_dir_path} using {template_path}")
+
+    files = [file for file in os.listdir(dir_path_content) if os.path.isfile(os.path.join(dir_path_content, file))]
+
+    nested_dirs = [dir for dir in os.listdir(dir_path_content) if os.path.isdir(os.path.join(dir_path_content, dir))]
+
+    for file in files:
+        if file.endswith(".md"):
+            full_path = os.path.join(dir_path_content, file)
+            dest_file_name = os.path.join(dest_dir_path, "index.html")
+            print(f"Creating page using {full_path}. Writing to {dest_dir_path}")
+            generate_page(full_path, template_path, dest_file_name)
+
+    for dir in nested_dirs:
+        new_source = os.path.join(dir_path_content, dir)
+        new_dest = os.path.join(dest_dir_path, dir)
+
+        shutil.rmtree(new_dest, ignore_errors=True)
+
+        os.mkdir(new_dest)
+
+        if os.path.exists(new_dest):
+            print(f"{new_dest} successfully created")
+
+        generate_pages_recursive(new_source, template_path, new_dest)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 #from htmlnode import HTMLNode
 #from textnode import *
 from copy_source_dest import copy_source_dest
-from generatepage import generate_page
+from generatepage import *
 
 def main():
     copy_source_dest("static", "public")
 
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "public")
 
 main()
